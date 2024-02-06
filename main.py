7326045380a431cc98245abec482d7aa4776e8cb
flo <escure.f@gmail.com>
2024-02-07 00:10:17 +0100

    propre

--- main.py
+++ main.py
@@ -12,14 +12,35 @@ import datetime
 import tkinter as tk
 from tkinter import ttk
 from datetime import datetime
+import shutil
+
 
 SIZE = 50
 POLICE = "Bangers-Regular.ttf" 
 Image.MAX_IMAGE_PIXELS = None
+distance_velo = 0.00000001
+distance_course = 0.0000001
+vitesse_velo = 0.0000001
+vitesse_course = 0.0000001
+altitude_velo = 0.0000001
+altitude_course = 0.0000001
+nb_velo = 0
+nb_course = 0
+ANNEE = None
+repertoire = "unzip/activities"
 
+
+image_couleur = Image.open("color.jpg")
+img_speed = (Image.open("speed.png"))
+img_climb = (Image.open("climb.png"))
+img_time = (Image.open("time.png"))
+img_climb = (Image.open("climb.png"))
+run = (Image.open("run.png"))
+velo = (Image.open("velo.png"))
 
 month_mapping = {
     'jan.': '01',
+    'janv.': '01',
     'févr.': '02',
     'fÃ©vr.': '02',
     'mars': '03',
@@ -32,16 +53,27 @@ month_mapping = {
     'sept.': '09',
     'oct.': '10',
     'nov.': '11',
-    'dec.': '12'
+    'dec.': '12',
+    'déc.': '12',
 }
 
-def convert_date(daate):
-    date_parts = daate.split()
-    day = date_parts[0]
-    month = month_mapping[date_parts[1]]
-    year = date_parts[2]
-    formatted_date = f"{day}-{month}-{year}"
-    return formatted_date
+def extract_zip_file():
+    current_directory = os.getcwd()
+    zip_files = [f for f in os.listdir(current_directory) if f.endswith('.zip')]
+    if not zip_files:
+        print("Aucun fichier zip trouvé dans le répertoire courant.")
+        return
+
+    zip_file = zip_files[0]
+    unzip_directory = 'unzip'
+    
+    if os.path.exists(unzip_directory):
+        shutil.rmtree(unzip_directory)
+    os.mkdir(unzip_directory)
+
+    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+        zip_ref.extractall(unzip_directory)
+    print("Contenu du fichier zip extrait avec succès dans le dossier 'unzip'.")    
 
 def csv_to_array(nom_fichier):
     donnees = []
@@ -102,6 +134,14 @@ def plot_gpx(gpx_file, dist):
     # Enregistrer l'image en PNG
     return image
 
+def convert_date(daate):
+    date_parts = daate.split()
+    day = date_parts[0]
+    month = month_mapping[date_parts[1]]
+    year = date_parts[2]
+    formatted_date = f"{day}-{month}-{year}"
+    return formatted_date
+
 def affichage_heure_minutes(secondes):
     minutes =  secondes//60
     heures = int(minutes // 60)  # Conversion en heures
@@ -146,13 +186,6 @@ def activity_data_image(date, distance, duree, montee, speed):
     
     return color_background (image)
 
-def unzip_file():
-    chemin_repertoire = 'EXPORT_ZIP'
-    fichiers_zip = glob.glob(f'{chemin_repertoire}/*.zip')
-    for fichier_zip in fichiers_zip:
-        with zipfile.ZipFile(fichier_zip, 'r') as zip_ref:
-            zip_ref.extractall(chemin_repertoire)
-
 def lire_balise_type_gpx(nom_fichier):
     try:
         with open(nom_fichier, 'r') as fichier:
@@ -172,75 +205,48 @@ def lire_balise_type_gpx(nom_fichier):
         print("Erreur : Impossible de lire le fichier GPX.")
         return None
 
+def resize_image(image, max_width, max_height):
+    original_width, original_height = image.size
+    width_ratio = max_width / original_width
+    height_ratio = max_height / original_height
+    resize_ratio = min(width_ratio, height_ratio)
+    new_width = int(original_width * resize_ratio)
+    new_height = int(original_height * resize_ratio)
+    resized_image = image.resize((new_width, new_height), Image.LANCZOS)
+    final_image = Image.new("RGB", (max_width, max_height))
+    x = (max_width - new_width) // 2
+    y = (max_height - new_height) // 2
+    final_image.paste(resized_image, (x, y))
+    return final_image
+
+def combine_images(image1, image2, image3, output_path):
+    combined_image = Image.new('RGB', (20*SIZE, 20*SIZE))
+    image1 = ImageOps.flip(image1)
+    combined_image.paste(image1, (0, 0))
+    combined_image.paste(image2, (4*SIZE, 18*SIZE))
+    combined_image.paste(image3, (int(2.5*SIZE), int(18.2*SIZE)))
+    combined_image.save(output_path)
+
+def color_nb(image_noir_blanc):
+    color = image_couleur.resize((image_noir_blanc.width, image_noir_blanc.height))
+    masque = Image.new("L", color.size)
+    masque.paste(image_noir_blanc, (0, 0))
+    resultat = Image.new("RGB", color.size)
+    resultat.paste(color, (0, 0), masque)
+    return resultat
+
+def color_background (img_to_color):
+    width, height = img_to_color.size
+    b = image_couleur.resize((width, height), Image.LANCZOS)
+    a = color_nb(img_to_color )
+    return a
+
 def type_image(type):
     if type == "9":
         return run
     
     return velo
     
-def resize_image(image, max_width, max_height):
-
-    # Récupérer les dimensions de l'image d'origine
-    original_width, original_height = image.size
-
-    # Calculer le ratio de redimensionnement pour s'adapter à la taille maximale
-    width_ratio = max_width / original_width
-    height_ratio = max_height / original_height
-
-    # Sélectionner le ratio de redimensionnement le plus petit pour s'adapter à la taille maximale
-    resize_ratio = min(width_ratio, height_ratio)
-
-    # Calculer les nouvelles dimensions de l'image
-    new_width = int(original_width * resize_ratio)
-    new_height = int(original_height * resize_ratio)
-
-    # Redimensionner l'image avec le nouveau ratio
-    resized_image = image.resize((new_width, new_height), Image.LANCZOS)
-
-    # Créer une nouvelle image blanche de la taille maximale
-    final_image = Image.new("RGB", (max_width, max_height))
-
-    # Calculer les coordonnées de l'endroit où placer l'image redimensionnée
-    x = (max_width - new_width) // 2
-    y = (max_height - new_height) // 2
-
-    # Insérer l'image redimensionnée dans la nouvelle image
-    final_image.paste(resized_image, (x, y))
-
-    # Enregistrer l'image finale
-    return final_image
-
-def combine_images(image1, image2, image3, output_path):
-
-
-    # Créer une nouvelle image avec un fond blanc de dimensions 1000x1000
-    combined_image = Image.new('RGB', (20*SIZE, 20*SIZE))
-    image1 = ImageOps.flip(image1)
-    # Placer la première image en haut
-    combined_image.paste(image1, (0, 0))
-
-    # Placer la deuxième image en bas à droite
-    combined_image.paste(image2, (4*SIZE, 18*SIZE))
-
-    # Placer la troisième image en bas à gauche
-    combined_image.paste(image3, (int(2.5*SIZE), int(18.2*SIZE)))
-
-    combined_image.save(output_path)
-
-def color_nb(image_noir_blanc):
-
-    color = image_couleur.resize((image_noir_blanc.width, image_noir_blanc.height))
-
-    # Conversion en masque avec canal alpha
-    masque = Image.new("L", color.size)
-    masque.paste(image_noir_blanc, (0, 0))
-
-    # Application du masque à l'image couleur
-    resultat = Image.new("RGB", color.size)
-    resultat.paste(color, (0, 0), masque)
-
-    return resultat
-
 def lire_activite(colone, ligne):
     y=0
     length = 5000
@@ -250,6 +256,38 @@ def lire_activite(colone, ligne):
             y=li
             break
     return activites[y][colone]
+
+def create_all(nom_fichier):
+    global distance_velo
+    global distance_course
+    global vitesse_velo
+    global vitesse_course
+    global altitude_velo
+    global altitude_course
+    global nb_velo
+    global nb_course
+    date_moche = lire_activite(1, nom_fichier[:-4]).replace("Ã\xa0", "a")
+    annee = date_moche.split()[2]
+    if annee != ANNEE :
+        return
+    print (date_moche)
+    img1 = plot_gpx(repertoire + "/" + nom_fichier, lire_activite(6, nom_fichier[:-4]))
+    img2 = activity_data_image(lire_activite(1, nom_fichier[:-4]), lire_activite(6, nom_fichier[:-4]), lire_activite(15, nom_fichier[:-4]), lire_activite(19, nom_fichier[:-4]), lire_activite(18, nom_fichier[:-4]))
+    img3 = type_image(lire_balise_type_gpx(repertoire+"/" + nom_fichier))
+    im1 = resize_image(img1,20*SIZE, 18*SIZE)
+    im3 = resize_image(img3,75, 75)
+    combine_images(im1, img2, im3, "IMAGES/"+nom_fichier[:-4]+".png")
+    type = lire_balise_type_gpx(repertoire+"/" + nom_fichier)
+    if type == "1" : 
+        distance_velo = distance_velo + float(lire_activite(6, nom_fichier[:-4]))
+        altitude_velo = altitude_velo + float(lire_activite(19, nom_fichier[:-4]))
+        vitesse_velo = vitesse_velo + float(lire_activite(18, nom_fichier[:-4]))
+        nb_velo = nb_velo+1
+    if type == "9" : 
+        distance_course =distance_course + float(lire_activite(6, nom_fichier[:-4]))
+        altitude_course = altitude_course + float(lire_activite(19, nom_fichier[:-4]))
+        vitesse_course = vitesse_course + float(lire_activite("Vitesse moyenne", nom_fichier[:-4]))
+        nb_course = nb_course+1
 
 def planche(colones, lignes):
     total_annee()
@@ -287,108 +325,6 @@ def planche(colones, lignes):
                 board[y:y + image_size[1], x:x + image_size[0]] = image
     cv2.imwrite("planche.png", board)
 
-color_img = Image.open("color.jpg")
-
-def color_background (img_to_color):
-    width, height = img_to_color.size
-    b = color_img.resize((width, height), Image.LANCZOS)
-    a = color_nb(img_to_color )
-    return a
-        
-def create_all(nom_fichier):
-    #print("nom fichier" + nom_fichier)
-    global distance_velo
-    global distance_course
-    global vitesse_velo
-    global vitesse_course
-    global altitude_velo
-    global altitude_course
-    global nb_velo
-    global nb_course
-    date_moche = lire_activite(1, nom_fichier[:-4]).replace("Ã\xa0", "a")
-    annee = date_moche.split()[2]
-    if annee != ANNEE :
-        return
-    print (date_moche)
-    img1 = plot_gpx(repertoire + "/" + nom_fichier, lire_activite(6, nom_fichier[:-4]))
-    img2 = activity_data_image(lire_activite(1, nom_fichier[:-4]), lire_activite(6, nom_fichier[:-4]), lire_activite(15, nom_fichier[:-4]), lire_activite(19, nom_fichier[:-4]), lire_activite(18, nom_fichier[:-4]))
-    img3 = type_image(lire_balise_type_gpx(repertoire+"/" + nom_fichier))
-    im1 = resize_image(img1,20*SIZE, 18*SIZE)
-    im3 = resize_image(img3,75, 75)
-    combine_images(im1, img2, im3, "IMAGES/"+nom_fichier[:-4]+".png")
-    type = lire_balise_type_gpx(repertoire+"/" + nom_fichier)
-    if type == "1" : 
-        distance_velo = distance_velo + float(lire_activite(6, nom_fichier[:-4]))
-        altitude_velo = altitude_velo + float(lire_activite(19, nom_fichier[:-4]))
-        vitesse_velo = vitesse_velo + float(lire_activite(18, nom_fichier[:-4]))
-        nb_velo = nb_velo+1
-    if type == "9" : 
-        distance_course =distance_course + float(lire_activite(6, nom_fichier[:-4]))
-        altitude_course = altitude_course + float(lire_activite(19, nom_fichier[:-4]))
-        vitesse_course = vitesse_course + float(lire_activite("Vitesse moyenne", nom_fichier[:-4]))
-        nb_course = nb_course+1
-
-
-
-
-# Vérifiez si le répertoire "IMAGES" n'existe pas déjà
-if not os.path.exists('IMAGES'):
-    # Créez le répertoire "IMAGES" s'il n'existe pas encore
-    os.makedirs('IMAGES')
-
-
-
-# Vérifiez si le répertoire "IMAGES" n'existe pas déjà
-if not os.path.exists('EXPORT_ZIP'):
-    # Créez le répertoire "IMAGES" s'il n'existe pas encore
-    os.makedirs('EXPORT_ZIP')
-
-
-
-
-files = [f for f in os.listdir() if f.endswith(".zip")]
-for f in files:
-    os.rename(f, os.path.join('EXPORT_ZIP', f))
-
-unzip_file()
-
-
-
-
-
-
-
-CSV = 'EXPORT_ZIP/activities.csv'
-activites = csv_to_array(CSV)
-
-distance_velo = 0.00000001
-distance_course = 0.0000001
-vitesse_velo = 0.0000001
-vitesse_course = 0.0000001
-altitude_velo = 0.0000001
-altitude_course = 0.0000001
-nb_velo = 0
-nb_course = 0
-
-
-image_couleur = Image.open("color.jpg")
-
-img_speed = (Image.open("speed.png"))
-img_climb = (Image.open("climb.png"))
-img_time = (Image.open("time.png"))
-img_climb = (Image.open("climb.png"))
-run = (Image.open("run.png"))
-velo = (Image.open("velo.png"))
-
-img_speed = resize_image(img_speed,SIZE, SIZE)
-img_climb = resize_image(img_climb,SIZE, SIZE)
-img_time = resize_image(img_time,SIZE, SIZE)
-img_climb = resize_image(img_climb,SIZE, SIZE)
-img_run = resize_image(run,SIZE, SIZE)
-img_velo = resize_image(velo,SIZE, SIZE)
-
-ANNEE = None
-
 def choisir_annee():
     global ANNEE  # Permet d'accéder à la variable globale ANNEE
     def afficher_annee():
@@ -410,25 +346,7 @@ def choisir_annee():
     # Bouton pour afficher l'année sélectionnée
     bouton_afficher = tk.Button(fenetre, text="Afficher", command=afficher_annee)
     bouton_afficher.pack()
-
-    # Exécuter la boucle principale de l'interface graphique
     fenetre.mainloop()
-
-# Appeler la fonction pour ouvrir la fenêtre de sélection d'année
-choisir_annee()
-print("Année sélectionnée :", ANNEE)  # Affiche l'
-
-
-repertoire = "EXPORT_ZIP/activities"
-
-for nom_fichier in os.listdir(repertoire):
-    if nom_fichier.endswith(".gpx"):
-        create_all(nom_fichier)
-        #print(nom_fichier)
-
-
-
-
 
 def total_annee():
     image = Image.new('RGB', (20*SIZE,20*SIZE), (0,0,0))
@@ -450,18 +368,35 @@ def total_annee():
     image.paste(img_climb, (int(1.75*SIZE), int(10.25*SIZE)))
     image.paste(img_climb, (int(10.75*SIZE), int(10.25*SIZE)))
 
-
     image = color_nb(image)
     image.save("total.png")
 
-    
 
-if nb_course==0:
-    nb_course =1
-if nb_velo==0:
-    nb_velo = 1
+shutil.rmtree('IMAGES')
+os.makedirs('IMAGES')
+
+extract_zip_file()
+CSV = 'unzip/activities.csv'
+activites = csv_to_array(CSV)
+
+img_speed = resize_image(img_speed,SIZE, SIZE)
+img_climb = resize_image(img_climb,SIZE, SIZE)
+img_time = resize_image(img_time,SIZE, SIZE)
+img_climb = resize_image(img_climb,SIZE, SIZE)
+img_run = resize_image(run,SIZE, SIZE)
+img_velo = resize_image(velo,SIZE, SIZE)
+
+choisir_annee()
+print("Année sélectionnée :", ANNEE)
 
 
+for nom_fichier in os.listdir(repertoire):
+    if nom_fichier.endswith(".gpx"):
+        create_all(nom_fichier)
 
-planche(6, 6)
+nb_course = max(nb_course, 1)
+nb_velo = max(nb_velo, 1)
+
+planche(7, 7)
+
 color_nb(Image.open("planche.png")).save("color_planche.png")
